273eb779f11cdd24c8d3375f9c071e3821c28126
Nadezhda Bokareva <nadezhda.yureva@rt.ru>
2025-06-07 12:12:30 +0300

    Наполнила класс MainPage

--- src/test/java/pages/pom/MainPage.java
+++ src/test/java/pages/pom/MainPage.java
@@ -1,4 +1,14 @@
 package pages.pom;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MainPage {
+    @FindBy(xpath = "$(\"a[href='/register']\")")
+    public SelenideElement registerButton;
+    @FindBy(xpath = "$(\"a[href='/login']\")\n")
+    public SelenideElement loginButton;
+    @FindBy(xpath = "$(\"a[href='/logout']\")\n")
+    public SelenideElement logoutButton;
+
 }
---
+++ src/test/java/pages/pom/RegisterPage.java
@@ -0,0 +1,5 @@
+package pages.pom;
+
+public class RegisterPage {
+
+}
